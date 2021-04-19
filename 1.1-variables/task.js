03862e75165f6911970e6c8a0611120c2d1d6b4d
somebodyasd <koriakovtsev@gmail.com>
2021-04-19 18:51:52 +0700

    hw BJS-1.1

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -6,22 +6,24 @@
  */
 
 function averageMark(a, g, p) {
-	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	let algebra = a, geography = g, physycs = p , avgGrade;
+
+	avgGrade = (algebra + geography + physycs) / 3
+
+	return avgGrade;
 }
 
 function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
+	let myName = userName
+	message = `Привет, мир! Меня зовут ${myName}` 
     
-	return // ваш ответ;
+	return message;
 }
 
 function calculateFormula() {
     
-	// реализуйте здесь задание №3
+	let x = 2, y = 22, z = 0;
+	let result = x * y + 5 * z + x - 1;
     
-    return // ваш ответ;
+    return result;
 }
