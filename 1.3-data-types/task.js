b4885c20d06cb2f663fcdfb97057df458954e97e
somebodyasd <koriakovtsev@gmail.com>
2021-05-01 00:53:47 +0700

    hw 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,25 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let creditSum = amount - contribution;
+    let numberOfPaidMonths = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth());
+    if ((date.getDate() - new Date().getDate) < 0) {
+        numberOfPaidMonths++;
+    }
+    let p = percent / 12;
+    let monthlyFee = creditSum - (creditSum * (p + p / (Math.pow(1 + p, numberOfPaidMonths)-1)));
+    let totalAmount = numberOfPaidMonths * monthlyFee;
+    return totalAmount.toFixed(2);
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting
+    if (name.length === 0) {
+        name = 'Аноним';
+        greeting = 'Привет, мир! Меня зовут ' + name;
+        return greeting;
+    } else {
+        greeting = 'Привет, мир! Меня зовут ' + name;
+        return greeting;
+        };
 }
