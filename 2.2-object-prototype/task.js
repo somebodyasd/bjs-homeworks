746f748e935576fcabc1a737a7a258bcfdb50beb
somebodyasd <koriakovtsev@gmail.com>
2021-05-28 00:18:35 +0700

    hw 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,32 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function() {
+    a = this.toUpperCase().replace(/\s/g, '');
+    for (let i = 0; i < Math.floor(a.length / 2); i++) {
+        if (a[i] !== a[a.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+
+// "А роза упала на лапу Азора".isPalindrome();
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    if (!marks.length) return 0;
+    let averageMark = 0;
+    for (let i = 0; i < marks.length; i++) {
+        averageMark += marks[i]
+    }
+    return Math.round(averageMark / marks.length);
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = Date.now();
+    // const formatter = new Intl.DateTimeFormat("ru");
+    let diff = now - Date.parse(birthday);
+    let age = diff / 3.154e+10;
+
+    return verdict = age > 18 ? true : false;
 }
